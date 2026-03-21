a16eb784fc533cad64091985a654a4c0ed71cb8a
Olivia Norine <oliv@Olivs-Mac.local>
2026-03-22 00:21:20 +0700

    tambahan model untuk berita

--- ruangpeduli/ruangpeduli/urls.py
+++ ruangpeduli/ruangpeduli/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from profiles.views import SocietyProfileViewSet, OrphanageProfileViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/profiles/', include('profiles.urls')),
-]
+    path('api/content/',  include('content.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
