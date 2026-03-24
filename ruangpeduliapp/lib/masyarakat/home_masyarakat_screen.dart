93ae53b7188dae421314854a4a0f1b2603e540e3
Olivia Norine <oliv@Olivs-Mac.local>
2026-03-24 21:34:04 +0700

    push

--- ruangpeduliapp/lib/masyarakat/home_masyarakat_screen.dart
+++ ruangpeduliapp/lib/masyarakat/home_masyarakat_screen.dart
@@ -1,44 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-class HomeMasyarakatScreen extends StatelessWidget {
-  const HomeMasyarakatScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFFFF0F2),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 80,
-                height: 80,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFF43D5E),
-                  shape: BoxShape.circle,
-                ),
-                child: const Icon(Icons.people_rounded,
-                    color: Colors.white, size: 40),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Selamat Datang,\nMasyarakat!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xFF1A1A1A),
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'Homepage Masyarakat\n(dalam pengembangan)',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: Colors.grey),
-=======
 import 'package:ruangpeduliapp/masyarakat/search_screen.dart';
 import 'package:ruangpeduliapp/masyarakat/profile_screen.dart';
 import 'package:ruangpeduliapp/masyarakat/notification_screen.dart';
